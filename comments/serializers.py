7a41aaf3dfb7287fb8ed22a7927614bbeb68c634
ZeroCool989 <>
2023-11-29 23:56:32 +0000

    added JSON Web Tokens and configured them

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 from .models import Comment
 
+
 class CommentSerializer(serializers.ModelSerializer):
     """
-    Serializer for the Comment model.
-    Adds extra fields when returning a list of Comment instances.
+    Serializer for the Comment model
+    Adds three extra fields when returning a list of Comment instances
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
@@ -22,24 +23,10 @@ class CommentSerializer(serializers.ModelSerializer):
             'post', 'created_at', 'updated_at', 'content'
         ]
 
-class CommentDetailSerializer(serializers.ModelSerializer):
-    """
-    Detailed Serializer for the Comment model.
-    This can include additional fields or methods compared to CommentSerializer.
-    """
-    owner = serializers.ReadOnlyField(source='owner.username')
-    is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    
 
-    def get_is_owner(self, obj):
-        request = self.context['request']
-        return request.user == obj.owner
-
-    class Meta:
-        model = Comment
-        fields = [
-            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-            'post', 'created_at', 'updated_at', 'content',
-        ]
+class CommentDetailSerializer(CommentSerializer):
+    """
+    Serializer for the Comment model used in Detail view
+    Post is a read only field so that we dont have to set it on each update
+    """
+    post = serializers.ReadOnlyField(source='post.id')
