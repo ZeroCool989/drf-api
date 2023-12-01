60c6ac76fabf98b5ccd17482702a0c6e6190518f
ZeroCool989 <>
2023-12-01 23:25:43 +0000

    Update CORS regex to allow requests from any Codeanywhere subdomain

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -110,7 +110,7 @@ if 'CLIENT_ORIGIN' in os.environ:
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+       r"^https://.*\.codeanyapp\.com$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
